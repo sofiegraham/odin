a8755824ce3dfaa49c18904636ccabff27763281
sofiegraham <sofiegraham@hotmail.com>
2016-12-29 22:42:32 -0800

    Web101 file upload

---
+++ web101/webprojects/test-site/scripts/main.js
@@ -0,0 +1,30 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+	var mySrc = myImage.getAttribute('src');
+	if(mySrc === 'images/chick.jpg') {
+		myImage.setAttribute ('src','images/samus.jpg');
+	} else {
+		myImage.setAttribute ('src','images/chick.jpg');
+	}
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Do you like chickens ' + myName + '?';
+}
+
+if(!localStorage.getItem('name')) {
+	setUserName();
+} else {
+	var storedName = localStorage.getItem('name');
+	myHeading.textContent = 'Do you like chickens ' + storedName + '?';
+}
+
+myButton.onclick = function() {
+	setUserName();
+}
