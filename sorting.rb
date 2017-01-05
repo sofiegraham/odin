a2bf12e1e940ea99e74d2c345f1b53c8874cb159
sofiegraham <sofiegraham@hotmail.com>
2017-01-04 19:14:39 -0800

    Sort without the sort method

---
+++ sorting.rb
@@ -0,0 +1,43 @@
+def sorting array
+
+
+end
+
+array = [2,8,3,4,5,1]
+
+sorting array
+
+while array.length > 0
+	comparable = 1
+
+#Try to find the smallest first
+	if array[0] < array[comparable]
+		
+		if comparable == array.length #check if we have reached the end of the array
+			#remove the array[0] and add it to new array, START OVER RECURSIVE
+
+
+		end
+		comparable = comparable+1
+	elsif array[0] > array[comparable]
+
+end
+
+
+
+
+
+
+while i < array.length
+	current_item = array[i]
+	if current_item > array[i+1]
+		small_array = small_array.push array[i+1]
+
+	end
+
+end
+
+
+array.each
+
+if array
