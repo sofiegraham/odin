90bc9c0fddb4ecac3470fab2979f8171da9124aa
sofiegraham <sofiegraham@hotmail.com>
2017-02-04 15:04:41 -0800

    Dungeons, docs and tic-tac-toes

--- ruby/tic_tac_toe/tic_tac_toe.rb
+++ ruby/tic_tac_toe/tic_tac_toe.rb
@@ -3,80 +3,79 @@ class Game
 	def initialize(player1, player2)
 		@player_x = Player.new(player1, "X")
 		@player_o = Player.new(player2, "O")
-		@grid = {
+		@board = {
 			:x0 => " ", :x1 => "1", :x2 => "2", :x3 => "3",
-			:a0 => "A", :a1 = "▢", :a2 = "▢", :a3 = "▢",
-			:b0 => "B", :b1 = "▢", :b2 = "▢", :b3 = "▢",
-			:c0 => "C", :c1 = "▢", :c2 = "▢", :c3 => "▢"
+			:a0 => "A", :a1 => "▢", :a2 => "▢", :a3 => "▢",
+			:b0 => "B", :b1 => "▢", :b2 => "▢", :b3 => "▢",
+			:c0 => "C", :c1 => "▢", :c2 => "▢", :c3 => "▢"
 		}
-		@board = Array.new(3) { Array.new(3,"▢") }
 		@current_player = @player_x
-		self.take_turn
+		@game_status = "playing"
+		self.game_loop
 	end
 
 	def show_board(board)
 		system "clear"
-		puts board[0].join(" ")
-		puts board[1].join(" ")
-		puts board[2].join(" ")
+		puts "#{board[:x0]} #{board[:x1]} #{board[:x2]} #{board[:x3]}"
+		puts "#{board[:a0]} #{board[:a1]} #{board[:a2]} #{board[:a3]}"
+		puts "#{board[:b0]} #{board[:b1]} #{board[:b2]} #{board[:b3]}"
+		puts "#{board[:c0]} #{board[:c1]} #{board[:c2]} #{board[:c3]}"
 	end
 
 	class Player
 		attr_reader :name, :sigil
-
 		def initialize(name, sigil)
 			@name = name
 			@sigil = sigil
 		end
 	end
 
-	def take_turn
-		number_of_turns = 0
-		while number_of_turns < 10
-			show_board(@board)
-			puts "#{@current_player.name}, select a square."
-			input = gets.chomp.to_i
-			case input
-			when 0
-				if @board[0][0] != "▢"
-					@board[0][0] = @current_player.sigil
+	def game_loop
+		while @game_status = "playing"
+			error = ""
+			selection_made = false
+			while selection_made == false
+
+				show_board(@board)
+				puts "#{error}#{@current_player.name}, select a square."
+				input = gets.chomp.downcase.to_sym
+				
+				if @board[input] == "▢"
+					@board[input] = @current_player.sigil
+					selection_made = true
 				else
-					puts "You can't go there you ass badger."
+					error = "You can't go there buttface! Now, "
 				end
-			when 1 then @board[0][1] = @current_player.sigil
-			when 2 then @board[0][2] = @current_player.sigil
-			when 3 then @board[1][0] = @current_player.sigil
-			when 4 then @board[1][1] = @current_player.sigil
-			when 5 then @board[1][2] = @current_player.sigil
-			when 6 then @board[2][0] = @current_player.sigil
-			when 7 then @board[2][1] = @current_player.sigil
-			when 8 then @board[2][2] = @current_player.sigil
-			else puts "Fine, your turn get's skipped, LOSER!"
 			end
 
 			if win_check(@board, @current_player)
 				show_board(@board)
 				puts "#{@current_player.name} wins"
+				@game_status = "won"
 				exit
 			else
-				@current_player = @current_player==@player_x ? @player_o : @player_x
-				
+				if @board.values.any? { |value| value == "▢"  }
+					@current_player = @current_player==@player_x ? @player_o : @player_x
+				else
+					show_board(@board)
+					puts "Its a tie..."
+					@game_status = "tie"
+					exit
+				end	
 			end
-			number_of_turns += 1
 		end
-		puts "Too many turns - stop it!"
 	end
 
 	def win_check(board, current_player)
 		if
-			(board[0][0] == current_player.sigil && board[0][1] == current_player.sigil && board[0][2] == current_player.sigil) ||
-			(board[1][0] == current_player.sigil && board[1][1] == current_player.sigil && board[1][2] == current_player.sigil) ||
-			(board[2][0] == current_player.sigil && board[2][1] == current_player.sigil && board[2][2] == current_player.sigil) ||
-			(board[0][0] == current_player.sigil && board[1][0] == current_player.sigil && board[2][0] == current_player.sigil) ||
-			(board[0][1] == current_player.sigil && board[1][1] == current_player.sigil && board[2][1] == current_player.sigil) ||
-			(board[0][2] == current_player.sigil && board[1][2] == current_player.sigil && board[2][2] == current_player.sigil) ||
-			(board[0][0] == current_player.sigil && board[1][1] == current_player.sigil && board[2][2] == current_player.sigil) ||
-			(board[0][2] == current_player.sigil && board[1][1] == current_player.sigil && board[2][0] == current_player.sigil)
+			(board[:a1] == current_player.sigil && board[:a2] == current_player.sigil && board[:a3] == current_player.sigil) ||
+			(board[:b1] == current_player.sigil && board[:b2] == current_player.sigil && board[:b3] == current_player.sigil) ||
+			(board[:c1] == current_player.sigil && board[:c2] == current_player.sigil && board[:c3] == current_player.sigil) ||
+			(board[:a1] == current_player.sigil && board[:b1] == current_player.sigil && board[:c1] == current_player.sigil) ||
+			(board[:a2] == current_player.sigil && board[:b2] == current_player.sigil && board[:c2] == current_player.sigil) ||
+			(board[:a3] == current_player.sigil && board[:b3] == current_player.sigil && board[:c3] == current_player.sigil) ||
+			(board[:a1] == current_player.sigil && board[:b2] == current_player.sigil && board[:c3] == current_player.sigil) ||
+			(board[:a3] == current_player.sigil && board[:b2] == current_player.sigil && board[:c1] == current_player.sigil)
 			true
 		else
 			false
