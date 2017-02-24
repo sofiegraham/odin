c692b2b9ce64bcdbc216c5550bea04a6265a3fbe
sofiegraham <sofiegraham@hotmail.com>
2017-02-24 08:10:25 -0800

    Recursive merge sort

--- ruby/ruby_problems/recursion.rb
+++ ruby/ruby_problems/recursion.rb
@@ -193,25 +193,53 @@ puts "hi"
 #puts fib(8)
 
 
-def merge_sort(list1=[], list2=[], sorted=[])
-	i1 = 1
-	i2 = 1
-	isorted = 1
-	while i1 <= list1.length && i2 <= list2.length
-		if list1[0] < list2[0]
-			sorted << list1[0]
-			i1 +=1
+def merging(left=[], right=[], sorted=[])
+	i_left = 0
+	i_right = 0
+	i_sorted = 0
+	while (i_left < left.length) && (i_right < right.length)
+		if left[i_left] < right[i_right]
+			sorted << left[i_left]
+			i_left +=1
 		else
-			sorted << list2[0]
-			i2 += 1
+			sorted << right[i_right]
+			i_right += 1
 		end
-		isorted += 1
-		if i1 == list1.length + 1
-	if list1.length >= 1
-		first_list = list1.pop(list1.length/2)
-		second_list = list1
+		i_sorted += 1
+	end
+	if i_left == left.length
+		while i_right < right.length
+			sorted << right[i_right]
+			i_right += 1
+		end
+	elsif i_right == right.length
+		while i_left < left.length
+			sorted << left[i_left]
+			i_left += 1
+		end
+	end
+	sorted
+end
+
+def sorting(list=[])
+	#byebug
+	if list.length <= 1
+		return list
+	else
+		first_list = list[0..((list.length/2)-1)]
+		second_list = list[(list.length/2)..-1]
+
+		return merging(sorting(first_list), sorting(second_list))
+	end
 
 end
+
+print merging([6,1,8,4,6,9,4,9,8])
+
+
+
+
+=begin
 
 def merge_sort(list1, list2=[], sorted=[])
 	if list1.length <= 1
@@ -255,7 +283,7 @@ end
 
 puts merge_sort([3,6,8,3,9,1,3])
 
-=begin
+
 
 def fib(n)
 	return n if n <= 1
@@ -368,4 +396,43 @@ def reverse_append(array, n)
 end
 
 print reverse_append([], 5)
+
+
+# This is a good merge sort
+def merge_sort(ary = [])
+	return ary if ary.size == 1
+
+	## Divide
+	a1 = ary[0...ary.size/2]
+	a2 = ary[ary.size/2..-1]
+
+	## Conquer
+	a1 = merge_sort(a1)
+	a2 = merge_sort(a2)
+
+	## Merge
+	result = []
+	idx1, idx2 = 0, 0
+
+	while result.size < (a1.size + a2.size)
+		if idx1 == a1.size
+			(result << a2[idx2..-1]).flatten!
+		elsif idx2 == a2.size
+			(result << a1[idx1..-1]).flatten!
+		elsif a1[idx1] <= a2[idx2]
+			result << a1[idx1]
+			idx1 += 1
+		else
+			result << a2[idx2]
+			idx2 += 1
+		end
+	end
+
+	return result
+end
+		
+a = [1, 3, 2, 10, -9, 12, 4, 3, 2, 4, 1223, 44, 9, 12, 8]
+p merge_sort(a)
+
+
 =end
